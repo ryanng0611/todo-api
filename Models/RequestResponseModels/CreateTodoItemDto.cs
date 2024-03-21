a2d37b622d0959890e4ffe9edc40cfa773107f5e
ryanng0611 <ryan.ng@snappymob.com>
2024-03-21 16:48:38 +0800

    Create custom DTO for creating item
    This change limits the data that can be modified or exposed through the API

---
+++ Models/RequestResponseModels/CreateTodoItemDto.cs
@@ -0,0 +1,8 @@
+namespace TodoApi.Models;
+
+public class CreateTodoItemDto
+{
+    // public long Id { get; set; }
+    public string? Name { get; set; }
+    // public bool IsComplete { get; set; }
+}
