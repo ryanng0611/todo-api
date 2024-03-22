6672f5fd61aacf8f8a891d01419e2455b36bcd2c
ryanng0611 <ryan.ng@snappymob.com>
2024-03-22 16:53:17 +0800

    Declare properties as nullable values

--- Models/DTOs/CreateUserDto.cs
+++ Models/DTOs/CreateUserDto.cs
@@ -2,7 +2,7 @@ namespace TodoApi.Models;
 
 public class CreateUserDto
 {
-    public string DisplayName { get; set; }
-    public string UserName { get; set; }
-    public string Password { get; set; }
+    public string? DisplayName { get; set; }
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
 }
--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -12,5 +12,5 @@ public class TodoItem
 
     [ForeignKey("User")]
     public Guid UserId { get; set; }
-    public virtual User User { get; set; }
+    public virtual User? User { get; set; }
 }
