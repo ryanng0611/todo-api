337f262b40ab9433f715cf08dc7dc7cb8bdc8198
ryanng0611 <ryan.ng@snappymob.com>
2024-03-22 16:46:19 +0800

    Update TodoItem and User models with proper relations in database

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace TodoApi.Models;
 
 public class TodoItem
 {
+    [Key]
     public long Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
+
+    [ForeignKey("User")]
+    public Guid UserId { get; set; }
+    public virtual User User { get; set; }
 }
---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+namespace TodoApi.Models;
+
+public class User
+{
+    [Key]
+    public Guid UserId { get; set; }
+    public string? DisplayName { get; set; }
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
+    [StringLength(10)]
+    public UserRole Role { get; set; }
+    [DataType(DataType.DateTime)]
+    public DateTime CreatedAt { get; set; }
+    public bool isDeleted { get; set; }
+    public virtual ICollection<TodoItem>? TodoItems { get; set; }
+}
