eebf09ecc230c506b86478d3ba762a9f38713bb8
ryanng0611 <ryan.ng@snappymob.com>
2024-03-25 16:55:33 +0800

    Enhance application startup configuration and add JWT authentication middleware
    - Configured AppSettings from appsettings.json for JWT secret.
    - Added SwaggerGen configuration for API documentation, including JWT authorization support.
    - Integrated JWT authentication middleware into the request pipeline.

--- Program.cs
+++ Program.cs
@@ -1,22 +1,60 @@
-using System.Configuration;
 using Microsoft.EntityFrameworkCore;
-using TodoApi.Models;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.OpenApi.Models;
 using TodoApi.Database;
+using TodoApi.Middleware;
+using TodoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.Configure<AppSettings>(
+    builder.Configuration.GetSection("AppSettings"));
 builder.Services.AddDbContext<DatabaseContext>(opt => opt
         .UseNpgsql(
         builder.Configuration
         .GetConnectionString
         ("PostgresDBConnection")));
+
 builder.Services.AddScoped<ITodoItemService, TodoItemService>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddSingleton<IUserServiceFactory, UserServiceFactory>();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    swagger =>
+    {
+        //This is to generate the Default UI of Swagger Documentation
+        swagger.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "TodoAPI",
+            Description = ".NET 8 Web API"
+        });
+        // To Enable authorization using Swagger (JWT)
+        swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+        });
+        swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+    }
+);
 
 var app = builder.Build();
 
@@ -30,6 +68,10 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseAuthentication();
+
+app.UseMiddleware<JwtMiddleware>();
 
 app.MapControllers();
 
