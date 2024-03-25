fb4fb0bc52646b5ebaf8932ae88d11dbf7dfb5db
ryanng0611 <ryan.ng@snappymob.com>
2024-03-25 16:38:27 +0800

    Refactor UsersController with additional endpoints and authorization
    - Added login endpoint for user authentication
    - Updated create-user endpoint to include authorization and return a success message
    - Added get-all-users endpoint to retrieve all users
    - Added get-user endpoint to retrieve a specific user by ID
    - Added update-user endpoint with authorization to update user details
    - Updated route prefix from "api/[controller]" to "user" for consistency

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using TodoApi.Database;
+using TodoApi.Middleware;
 using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("user")]
     [ApiController]
     public class UsersController : ControllerBase
     {
@@ -21,17 +15,60 @@ namespace TodoApi.Controllers
             _userService = userService;
         }
 
-        [HttpPost]
-        public async Task<ActionResult<User>> CreateNewUser(CreateUserDto createUserDto)
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginUser(LoginUserDto loginUserDto)
+        {
+            var response = await _userService.LoginUser(loginUserDto);
+
+            if (response == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(response);
+        }
+
+        [HttpPost("create-user")]
+        [Authorize]
+        public async Task<ActionResult> CreateNewUser(CreateUserDto createUserDto)
         {
             var createdNewUser = await _userService.CreateUserAsync(createUserDto);
 
             if (createdNewUser == null)
             {
-                return NotFound("Something went wrong.");
+                return Conflict("User with this username already exists");
             }
 
-            return Ok(createdNewUser);
+            return Ok("User registered successfully.");
+        }
+
+        [HttpGet("get-all-users")]
+        public async Task<ActionResult<IEnumerable<User>>> GetAllUsers()
+        {
+            var userList = await _userService.GetUsersAsync();
+            return Ok(userList);
+        }
+
+        [HttpGet("get-user")]
+        public async Task<ActionResult<User>> GetUser(Guid userId)
+        {
+            var user = await _userService.GetUserAsync(userId);
+            if (user == null)
+            {
+                return NotFound($"User with id ({userId}) was not found.");
+            }
+            return Ok(user);
+        }
+
+        [HttpPut("update-user/{userId}")]
+        [Authorize]
+        public async Task<IActionResult> UpdateUser(Guid userId, UpdateUserDto updateUserDto)
+        {
+            var updatedTodoItem = await _userService.UpdateUserAsync(userId, updateUserDto);
+
+            if (updatedTodoItem == null)
+            {
+                return NotFound($"User with id ({userId}) was not found.");
+            }
+            return CreatedAtAction(nameof(GetUser), new { id = userId }, updatedTodoItem);
         }
     }
 }
