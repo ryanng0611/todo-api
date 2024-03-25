d7606cb96d8bd0fefa0bd69d8ccefa1390712156
ryanng0611 <ryan.ng@snappymob.com>
2024-03-25 16:30:28 +0800

    Migrate changes to User model from .NET CLI

---
+++ Migrations/20240325032613_UpdateUserModel_v3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserModel_v3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TodoItems_Users_UserId",
+                table: "TodoItems");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EditedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TodoItems_Users_UserId",
+                table: "TodoItems",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TodoItems_Users_UserId",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "EditedAt",
+                table: "Users");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TodoItems_Users_UserId",
+                table: "TodoItems",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
