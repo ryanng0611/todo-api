4f42c99eb35301af3495fcfa921aba41bfde1c60
ryanng0611 <ryan.ng@snappymob.com>
2024-03-26 11:10:14 +0800

    Initialize the property with a default value

--- Models/DTOs/CreateUserDto.cs
+++ Models/DTOs/CreateUserDto.cs
@@ -2,7 +2,7 @@ namespace TodoApi.Models;
 
 public class CreateUserDto
 {
-    public string? DisplayName { get; set; }
-    public string? UserName { get; set; }
-    public string? Password { get; set; }
+    public string DisplayName { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 }
--- Models/DTOs/LoginUserDto.cs
+++ Models/DTOs/LoginUserDto.cs
@@ -1,6 +1,6 @@
 namespace TodoApi.Models;
 public class LoginUserDto
 {
-    public string? UserName { get; set; }
-    public string? Password { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 }
